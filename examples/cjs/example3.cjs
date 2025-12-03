dbdf95edf44e039e4edf2a6f7a80823625c7c639
tduyng <tduyng.dev@gmail.com>
2025-12-03 16:20:44 +0100

    chore: remove dead comments and update breaking changes for node version

--- examples/cjs/example3.cjs
+++ examples/cjs/example3.cjs
@@ -2,7 +2,6 @@ const logger = require('../../lib/cjs/index.js')
 
 logger.setNamespaces('namespace:*')
 logger.setLevel('debug')
-//logger.setOutput(logger.outputs.json)
 
 const log = logger.createLogger('namespace:subNamespace')
 log.debug('ctxId', 'User login attempt', {
--- examples/esm/example3.js
+++ examples/esm/example3.js
@@ -2,7 +2,6 @@ import * as logger from '../../lib/esm/index.js'
 
 logger.setNamespaces('namespace:*')
 logger.setLevel('debug')
-//logger.setOutput(logger.outputs.json)
 
 const log = logger.createLogger('namespace:subNamespace')
 log.debug('ctxId', 'User login attempt', {
